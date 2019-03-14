2e641a93049ee739d5e610a9c5ce787b508c2599
mytechguyri <46641218+mytechguyri@users.noreply.github.com>
2019-03-13 20:24:53 -0400

    Add files via upload

--- dtmfdecoder.py
+++ dtmfdecoder.py
@@ -20,12 +20,24 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+## Note  In order for interrupts to work correctly, the following line in /boot/config.txt
+## needs to be changed
+##	dtoverlay=mcp23017,addr=0x20,gpiopin=12
+## change it to
+##	dtoverlay=mcp23017,addr-0x20,gpiopin=6
+## Then reboot the system.  This is an unused gpio pin on the pi.  The reason for the change is the
+## hardware overlay locks out interrupt detection on the real gpio pin, 12.
+## So by doing this, we trick the hardware overlay into not locking out interrupts on gpiopin 12
+
 import sys
 import subprocess
 import time
+import RPi.GPIO as GPIO
+import datetime
+GPIO.setmode(GPIO.BCM)
 
 #Setup default variables based on pinout of DTMF decoder board
-IRQ = 12		#GPIO Expander interrupt pin from /boot/config.txt
+IRQ = "gpio12"		#GPIO Expander interrupt pin
 Vcc = "gpio503"		#3.3vdc, just setting the GPIO high provides sufficient power to run the board
 Q1 = "gpio498"    	#DTMF = DTMF + 1
 Q2 = "gpio499"		#DTMF = DTMF + 2
@@ -34,12 +46,14 @@ Q4 = "gpio501"		#DTMF = DTMF + 8
 STQ2 = "gpio502" 	#Normally High, goes low when tone detected, then back high once tone stops.
 GPIOPath = "/sys/class/gpio/"
 
-#Export the GPIO pins we're going to use
-for x in range (498, 504):
+#Export the GPIO pins on bank A of the MCP23017 GPIO expander
+for x in range (496, 504):
 	subprocess.call("echo " + str(x) + " > /sys/class/gpio/export", shell=True)
 subprocess.call("echo 12 > /sys/class/gpio/export", shell=True)
-subprocess.call("echo both > /sys/class/gpio/gpio502/edge", shell=True)
-subprocess.call("echo out > " + str(GPIOPath) + str(Vcc) + "/direction", shell=True)
+subprocess.call("echo 1 > /sys/class/gpio/gpio12/active_low", shell=True)
+subprocess.call("echo falling > /sys/class/gpio/" + STQ2 + "/edge", shell=True)
+subprocess.call("echo out > /sys/class/gpio/" + Vcc + "/direction", shell=True)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 #Get DTMF_PTY path from supplemental config file
 svxlinkvars = {}
@@ -49,38 +63,44 @@ with open("/etc/svxlink/svxlink.d/dtmfdecoder.conf") as myfile:
                 svxlinkvars[name.strip()] = var[:-1]
 DTMFPty = str(svxlinkvars["DTMF_PTY"])
 
-
-#Define function to read GPIO pins
+#Define function to read individual GPIO pins
 def getvalue(pin):
-	cmd = "cat " + GPIOPath + pin + "/value"
+	cmd = "cat " + GPIOPath + str(pin) + "/value"
 	bit = subprocess.check_output(cmd, shell = True)
 	return int(bit)
 
 #Define function to convert binary coded decimal to keypress
-def getDigit(Q1,Q2,Q3,Q4):
-	bits = [getvalue(Q1),getvalue(Q2),getvalue(Q3),getvalue(Q4)]
-	dtmf = ["D","1","2","3","4","5","6","7","8","9","0","*","#","A","B","C"]
-	code = 0
-	code = (bits[0] * 1) + (bits[1] * 2) + (bits[2] * 4) + (bits[3] * 8)
-	Key = str(dtmf[code])
-	return str(Key)
+def getDigit():	#Callback when interrupt goes low
+	if not getvalue(STQ2):														#STQ2 is low when tone is detected
+		bits = [getvalue(Q1),getvalue(Q2),getvalue(Q3),getvalue(Q4)]								#Get binary coded decimal from MT8870
+		dtmf = ["D","1","2","3","4","5","6","7","8","9","0","*","#","A","B","C"]						#Array of possible key presses
+		code = 0														#zero out any previous attempts
+		code = (bits[0] * 1) + (bits[1] * 2) + (bits[2] * 4) + (bits[3] * 8)							#Convert binary coded decimal to decimal
+		Key = str(dtmf[code])													#Convert decimal to dtmf digit pressed from array
+		logentry = datetime.datetime.now().strftime("%a %b %d %H:%M:%S %Y") + ": MT8870 DTMF Decoder: digit=" + str(Key)	#Generate log entry
+		subprocess.call('echo "' + logentry + '" >> /var/log/svxlink', shell=True)   						#Log successful hardware DTMF detection to SVXLink log
+		subprocess.call('echo "' + str(Key) + '" > ' + str(DTMFPty), shell=True)						#Send the digit to svxlink
+	else:
+		subprocess.call('echo  " " > ' + str(DTMFPty), shell=True)								#Clear digit sent to svxlink when tones end and STQ2 goes high
+	return
+
+callback = lambda self : getDigit()
+GPIO.add_event_detect(12, GPIO.RISING, callback=callback, bouncetime=100)   								#Capture interrupt event
 
 #Initialize the DTMF Decoder board by power cycling the Vcc pin
-subprocess.call('echo 0 > ' + GPIOPath + Vcc + '/value', shell=True)
+subprocess.call('echo 0 > ' + GPIOPath + Vcc + '/value', shell = True)
 time.sleep(1)
-subprocess.call('echo 1 > ' + GPIOPath + Vcc + '/value', shell=True)
+subprocess.call('echo 1 > ' + GPIOPath + Vcc + '/value', shell = True)
+getvalue(STQ2)																#Clear interrupts from initial setup by reading pin
+
 
 #Main program loop
 while True:
 
 	try:
-		while getvalue(STQ2):	#Wait for tone to be detected (pin will go low)
-			time.sleep(.05)
-		DTMFKey = getDigit(Q1,Q2,Q3,Q4)	#Read the 4 GPIO decoder pins and decode
-		subprocess.call("echo '" + str(DTMFKey) + "' > " + str(DTMFPty), shell=True)	#Send digit to svxlink
-		while not getvalue(STQ2):	#Wait for tone to stop (pin  will go high)
-			time.sleep(.05)
-		subprocess.call("echo ' ' > " + str(DTMFPty), shell=True)	#Send space to let svxlink know tone has stopped
+		time.sleep(10)
 	except KeyboardInterrupt:
+		GPIO.cleanup()
 		sys.exit(1)
+GPIO.cleanup()
 
